267e346a217df16be947db8f48192bb1d6696cf4
m3hrad <mehrad.mohammadi@futurice.com>
2018-10-02 17:54:02 +0300

    today functionality added

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+let moment = require('moment');
+
 
 export default class Day extends React.Component {
   isEven(value){
     return (value % 2) === 0;
   }
 
+  capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   render() {
+    const today = moment().format('dddd');
+    const capitalizedDay = this.capitalizeFirstLetter(this.props.day);
+    let todayComponent = '';
+    if (capitalizedDay === today) {
+      todayComponent = <span>TODAY</span>;
+
+    }
+
+
     let displayedHours = '';
     let hours = this.props.hours;
 
     if (hours === undefined || hours.length === 0) {
-      displayedHours = 'closed';
+      displayedHours = 'Closed';
     }
     else {
       displayedHours = this.props.hours.map((hour,index) => {
@@ -27,7 +42,8 @@ export default class Day extends React.Component {
 
     return (
       <div>
-        {this.props.day}
+        {capitalizedDay}
+        {todayComponent}
         {displayedHours}
       </div>
     );
